470e6b4ebe6a12d6ee652bc7f13bbdced5d69208
yshiraz <yahya.20241888@iit.ac.lk>
2025-12-29 12:44:24 +0530

    Add CLI input for study session details

--- main.py
+++ main.py
@@ -1,1 +1,10 @@
 print("Smart Study and Productivity Analyzer")
+
+date = input("Enter date (YYYY-MM-DD): ")
+subject = input("Enter subject: ")
+duration = input("Enter study duration (minutes): ")
+time_of_day = input("Enter Time of Day (Morning/Afternoon/Night): ")
+focus = input("Focus level (1-5): ")
+
+print("\nStudy Session recorded: ")
+print(date, subject, duration, time_of_day, focus)
